17a7660afb34becf43da905ef1d7ab52c6fb0010
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    r3q: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -132,12 +132,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     hwcomposer.msmnile \
-    memtrack.msmnile \
     gralloc.msmnile
 
 PRODUCT_PACKAGES += \
